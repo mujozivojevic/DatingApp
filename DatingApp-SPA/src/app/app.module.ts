42045be4fe848bcbec33da5cfed96952527dd514
Mujo Živojević <mujo.zivojevic@edu.fit.ba>
2020-05-03 16:22:05 +0200

    Adding Action Filters

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -30,7 +30,7 @@ import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
-
+import { TimeagoModule } from 'ngx-timeago';
 
 
 export function tokenGetter() {
@@ -60,6 +60,7 @@ export function tokenGetter() {
       BsDatepickerModule.forRoot(),
       BsDropdownModule.forRoot(),
       TabsModule.forRoot(),
+      TimeagoModule.forRoot(),
       BrowserAnimationsModule,
       NgxGalleryModule,
       FileUploadModule,
